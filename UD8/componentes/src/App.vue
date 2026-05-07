662dc83e5b22a191b02f95c90e8042a42fd9c7fd
arodriguezgim <arodriguezg@iesch.org>
2026-05-07 17:58:12 +0200

    Fin Clase Jueves

---
+++ UD8/componentes/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import BotonContador from './components/BotonContador.vue';
+import BlogPost from './components/BlogPost.vue';
+import { ref } from 'vue';
+
+
+const posts = ref([
+  { id:1, title: "Post 01", body:"Contenido del post 01"},
+  { id:2, title: "Post 02", body:"Contenido del post 02"},
+  { id:3, title: "Post 03", body:"Contenido del post 03"},
+  { id:4, title: "Post 04"},
+  { id:5, title: "Post 05", body:"Contenido del post 05"},
+]);
+
+const miFavorito = ref("")
+
+const cambiarFavorito = ( titulo )=> {
+  miFavorito.value = titulo;
+}
+
+</script>
+
+
+
+<template>
+  <div class="container">
+      <h1>DAM 1 - 2026</h1>
+      <BotonContador />
+      <hr>
+      <h2>Mi Post Favorito: {{ miFavorito }}</h2>
+      <div class="container">
+        <BlogPost 
+          v-for="post in posts"
+          :key="post.id"
+          :title="post.title"
+          :body="post.body"
+          :id="post.id"
+          @cambiarTituloFavorito="cambiarFavorito"
+          class="mb-2"
+        />
+      </div>
+      
+
+
+      
+
+  </div>
+
+
+</template>
+
+<style scoped></style>
---
+++ UD8/componentes/src/components/BlogPost.vue
@@ -0,0 +1,35 @@
+<script setup>
+    // defineProps(['title', 'body', 'id', 'colorText'])
+    // String, Number, Boolean, Array, Object, Date, Function, Symbol
+    const props = defineProps({
+        title: String,
+        id: Number,
+        body: {
+            type: String,
+            default: 'Sin descripción'
+        }
+    })
+
+    const emit = defineEmits(["cambiarTituloFavorito"])
+
+    
+</script>
+<template>
+
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title" >
+                {{ title }}
+            </h4>
+            <p>
+                {{ id }} - {{ body }}
+            </p>
+            <button 
+                @click="emit('cambiarTituloFavorito', title )"
+                class="btn btn-outline-primary">
+                    Mi Favorito
+            </button>
+
+        </div>
+    </div>
+</template>
